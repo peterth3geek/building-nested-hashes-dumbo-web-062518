39263dd73c5823541bbf3202431a31226019f9c4
Peter Hargarten <peterwhargarten@gmail.com>
2018-06-22 19:56:58 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,17 +2,17 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague"
-        :age => "53"
+        :name => "Lord Montague",
+        :age => "53",
         },
       :matriarch => {
-        :name => "Lady Montague"
-        :age => "54"
+        :name => "Lady Montague",
+        :age => "54",
         },
       :hero => {
         #your key/value pairs here
-        :name => "Romeo"
-        :age => "15"
+        :name => "Romeo",
+        :age => "15",
         :status => "alive"
         },
       :hero_friends => []
@@ -20,18 +20,18 @@ def third_challenge
    :capulet => {
       :patriarch => {
         #your key/value pairs here
-        :name => "Lord Capulet"
-        :age => "50"
+        :name => "Lord Capulet",
+        :age => "50",
         },
       :matriarch => {
         #your key/value pairs here
-        :name = "Lady Capulet"
+        :name = "Lady Capulet",
         :age = "51"
         },
       :heroine => {
         #your key/value pairs here
-        :name => "Juliet"
-        :age => "15"
+        :name => "Juliet",
+        :age => "15",
         :status => "alive"
         },
       :heroine_friends => []
