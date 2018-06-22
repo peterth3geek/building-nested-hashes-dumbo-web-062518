c50357747e43fe8e6668a33425e5a0de5c4c4c77
Peter Hargarten <peterwhargarten@gmail.com>
2018-06-22 19:50:38 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,14 +3,14 @@ def second_challenge
     :montague => {
       :patriarch =>{}
       :matriarch =>{}
-      :hero = {}
+      :hero => {}
       :hero_friends{}
       },
     :capulet => {
-      :patriarch{}
-      :matriarch{}
-      :heroine{}
-      :heroine_friends{}
+      :patriarch =>{}
+      :matriarch => {}
+      :heroine => {}
+      :heroine_friends =>{}
       }}
 
 
